b37c1bfe6757039d28a8b2bf3a0f1391fe3b06e2
Harman <harmansaluja02@gmail.com>
2023-12-08 16:07:11 +0530

    Final design made, no changes in setup and backend

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -36,7 +36,7 @@ export default function Login() {
   }
 
   return (
-    <div style={{backgroundImage: 'url("https://images.pexels.com/photos/326278/pexels-photo-326278.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', height: '100vh', backgroundSize: 'cover' }}>
+    <div style={{backgroundImage: 'url("https://wallpapers.com/images/featured/medical-background-cjge7e89adg6ub8x.webp")', height: '100vh', backgroundSize: 'cover' }}>
       <div>
         <Navbar />
       </div>
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -65,7 +65,7 @@ export default function Signup() {
   }
 
   return (
-    <div style={{ backgroundImage: 'url("https://images.pexels.com/photos/1565982/pexels-photo-1565982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")', backgroundSize: 'cover',height: '100vh' }}>
+    <div style={{ backgroundImage: 'url("https://wallpapers.com/images/hd/medical-background-cjge7e89adg6ub8x.jpg")', backgroundSize: 'cover',height: '100vh' }}>
       <div>
       <Navbar />
       </div>
